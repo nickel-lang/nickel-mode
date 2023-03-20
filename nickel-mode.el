296cd7ffe495848a537e1f1c7fc366eef5fa630d
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2023-03-20 15:15:44 +0100

    Add support for better font locking

--- nickel-mode.el
+++ nickel-mode.el
@@ -25,13 +25,78 @@
   :link '(url-link :tag "Site" "https://nickel-lang.org/")
   :link '(url-link :tag "Repository" "https://github.com/nickel-lang/nickel-mode"))
 
-(define-derived-mode nickel-mode prog-mode
-  "Nickel"
-  "Major mode for editing Nickel source code."
-  :group 'nickel)
+(defvar nickel-mode-keywords
+  (regexp-opt '("if" "then" "else" "forall" "in" "let"
+                "rec" "match" "fun" "import" "merge"
+                "default" "doc" "optional" "priority" "force" "not_exported")
+	      'word))
+
+(defvar nickel-mode-constants
+  (regexp-opt '("true" "false" "null")
+	     'word))
+
+(defvar nickel-mode-types
+  (regexp-opt '("Dyn" "Num" "Str" "Array")
+	      'word))
+
+(defvar nickel-mode-primops
+  (regexp-opt '("%typeof%" "%assume%" "%array_lazy_assume%" "%dictionary_assume%"
+	        "%blame%" "%chng_pol%" "%polarity%" "%go_dom%" "%go_codom%" "%go_field%"
+	        "%go_array%" "%go_dict%" "%seal%" "%unseal%" "%embed%" "%record_map%"
+	        "%record_insert%" "%record_remove%" "%record_empty_with_tail%" "%record_seal_tail%"
+	        "%record_unseal_tail%" "%seq%" "%deep_seq%" "%force%" "%head%" "%tail%" "%length%"
+	        "%fields%" "%values%" "%pow%" "%trace%" "%has_field%" "%map%" "%elem_at%" "%generate%"
+	        "%rec_force%" "%rec_default%")
+	      'symbol))
+
+
+(defconst nickel-mode-identifiers "\\_<?[a-zA-Z][_a-zA-Z0-9-']*\\_>")
+;; Same as identifiers, but starting with a `
+;; TODO: Not sure if the ` will work yet...
+(defconst nickel-mode-enum-tags "\\_<`_?[a-zA-Z][_a-zA-Z0-9-']*\\_>")
+(defconst nickel-mode-numbers "\\_<[0-9]*\\.?[0-9]+\\_>")
+(defconst nickel-mode-operators
+  (regexp-opt
+    '("->" "\\[" "]" "," "|" ":" "=" "==" "|>"
+      "!=" ")" "&&" "||" "{" "}" "(" "?" ";" "$" "&" "."
+      "\"" "+" "-" "*" "/" "%" "@" "!" ".." "=>" "_" "<" ">"
+      "<=" ">=" "[|" "|]")))
+;; TODO
+(defconst nickel-mode-strings "")
+
+(defconst nickel-mode-font-lock-keywords
+  `(
+    (,nickel-mode-keywords . font-lock-keyword-face)
+    (,nickel-mode-types . font-lock-type-face)
+    (,nickel-mode-constants . font-lock-constant-face)
+    (,nickel-mode-primops . font-lock-builtin-face)
+    (,nickel-mode-enum-tags . font-lock-constant-face)    
+    (,nickel-mode-identifiers . (1 font-lock-variable-name-face))
+    (,nickel-mode-operators . font-lock-builtin-face)
+    (,nickel-mode-numbers . font-lock-constant-face)))
 
 ;; Use Nickel mode for .ncl files
 (add-to-list 'auto-mode-alist '("\\.ncl\\'" . nickel-mode))
+
+(defvar nickel-mode-syntax-table
+  (let ((st (make-syntax-table)))    
+    ;; Handle comments
+    (modify-syntax-entry ?# "<" st)
+    (modify-syntax-entry ?\n ">" st)
+    st))
+
+
+(define-derived-mode nickel-mode prog-mode
+  "Nickel"
+  "Major mode for editing Nickel source code."
+  
+  (setq font-lock-defaults '((nickel-mode-font-lock-keywords)))
+
+  ;; (setq-local comment-start "# ")
+  ;; (setq-local comment-end "")
+  (set-syntax-table nickel-mode-syntax-table)
+   
+  :group 'nickel)
 
 (provide 'nickel-mode)
 
